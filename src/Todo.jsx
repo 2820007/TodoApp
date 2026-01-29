94c7eebfd102c8142b93a59595f08a18c9e80a8e
Ravi Mandal <2820007.cse@pietgroup.co.in>
2026-01-29 15:44:23 +0545

    addTodo added

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,63 +1,122 @@
-import React, { useState } from "react";
+import { useContext, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { TodoContext } from "./context/TodoProvider";
 
-const Todo = () => {
+function Todo() {
   const [todo, setTodo] = useState("");
   const [todoError, setTodoError] = useState("");
-
+  const { state, dispatch } = useContext(TodoContext);
   const addTodo = () => {
-    if (todo.trim().length === 0) {
-      setTodoError("Task must contain some characters!");
+    if (todo.length <= 0) {
+      setTodoError("Most Be Some characters in the input field !* ");
       return;
     }
-
-    console.log(todo); 
+    dispatch({
+      type: "addTodo",
+      payload: {
+        id: uuidv4(),
+        title: todo,
+      },
+    });
     setTodo("");
-    setTodoError("");
   };
 
+  console.log(state.todos);
   return (
-    <div className="bg-gray-200 w-[60%] m-auto mt-20 flex flex-col gap-y-6">
-      <div className="flex justify-center mt-4 p-6 gap-x-4">
-        <input
-          value={todo}   
-          onChange={(e) => {
-            setTodo(e.target.value);
-            setTodoError(""); 
-          }}
-          className="w-100 bg-white outline-none rounded-sm p-4 shadow"
-          type="text"
-          placeholder="enter your task"
-        />
+    <div className=" w-[70%]   m-auto mt-20 p-5 bg-gray-200">
+      <div className="      ">
+        <div className=" gap-x-4   flex  justify-center ">
+          <label htmlFor="">
+            <input
+              value={todo}
+              onChange={(e) => {
+                setTodo(e.target.value);
+                setTodoError("");
+              }}
+              required
+              className="  bg-white shadow outline-none w-full p-3 rounded-2xl"
+              type="text"
+              placeholder="Enter Task.."
+            />
+          </label>
+          <button
+            onClick={() => {
+              addTodo();
+            }}
+            className="bg-orange-500 p-3  text-white rounded-sm cursor-pointer"
+          >
+            Add Todo
+          </button>
+        </div>
 
-        <button
-          onClick={addTodo}
-          className="bg-orange-600 text-white px-4 py-2 rounded-sm cursor-pointer"
-        >
-          Add Task
-        </button>
+        {todoError.length > 0 && (
+          <p className="  italic  font-bold text-sm text-center text-red-500 ">
+            {todoError}
+          </p>
+        )}
       </div>
 
-      {todoError && (
-        <p className="text-red-600 text-center font-medium">
-          {todoError}
-        </p>
-      )}
+      <div className="  space-y-4  p-4">
+        {state.todos.length > 0 ? (
+          <>
+            {state.todos.map((item) => {
+              return (
+                <div
+                  key={item.id}
+                  className="bg-white flex   justify-between   items-center shadow-2xl rounded-xl  px-5 p-3"
+                >
+                  <h1>{item.title}</h1>
 
-      <div className="space-y-4 p-4">
-        <div className="flex justify-between border bg-white shadow-2xl rounded-2xl px-5 p-3">
-          <h1>Task1</h1>
-          <div className="space-x-3.5">
-            <button className="bg-red-700 px-4 py-2 text-white rounded-sm">
-              Delete
-            </button>
-            <button className="bg-blue-700 text-white px-4 py-2 rounded-sm">
-              Edit
-            </button>
+                  <div className="space-x-3">
+                    <button
+                      onClick={() =>
+                        dispatch({
+                          type: "deleteTodo",
+                          payload: {
+                            id: item.id,
+                          },
+                        })
+                      }
+                      className="bg-red-500 p-2  rounded-xl  text-white cursor-pointer  "
+                    >
+                      Delete
+                    </button>
+                    <button
+                      onClick={() => {
+                        dispatch({
+                          type: "updateTodo",
+                          payload: {
+                            id: item.id,
+                            title: "New Title",
+                          },
+                        });
+                      }}
+                      className="bg-green-500 p-2  rounded-xl  text-white cursor-pointer  "
+                    >
+                      Edit
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
+          </>
+        ) : (
+          <div className="font-bold flex gap-1 justify-center text-2xl text-center italic text-red-700">
+            <h1>There is no todo to show </h1>
+            <img
+              className="rounded-full w-8"
+              src="
+         https://png.pngtree.com/png-vector/20241102/ourmid/pngtree-crying-sad-emoji-png-image_14216691.png            "
+              alt=""
+            />
+            !
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
-};
+}
 
 export default Todo;
+// crud
+// create, read , update , delete
--- src/context/TodoProvider.jsx
+++ src/context/TodoProvider.jsx
@@ -1,24 +1,54 @@
 import { createContext, useReducer } from "react";
 
-export const TodoContext=createContext()
+export const TodoContext = createContext();
 
-const  initialState={}
-const todoReducer=(state,action)=>{
-    switch(action.type){
-        case "default":{
-            return state
-        }
+const initialState = {
+  todos: [],
+};
+
+const todoReducer = (state, action) => {
+  switch (action.type) {
+    case "addTodo": {
+      const isExist = state.todos.find((item) => {
+        return item.id == action.payload.id;
+      });
+      if (isExist) {
+        return state;
+      } else {
+        const newTodo = [...state.todos, action.payload];
+        return {
+          todos: newTodo,
+        };
+      }
+    }
+    case "deleteTodo": {
+      return {
+        ...state,
+        todos: state.todos.filter((item) => item.id !== action.payload.id),
+      };
+    }
+    case "updateTodo": {
+      return {
+        ...state,
+        todos: state.todos.map((item) =>
+          item.id === action.payload.id
+            ? { ...item, title: action.payload.title }
+            : item,
+        ),
+      };
     }
 
-}
+    case "default": {
+      return state;
+    }
+  }
+};
 
-export const TodoProvider=({children})=>{
-
-    const [state,dispatch]=useReducer(todoReducer,initialState)
-    return (
-        <TodoContext.Provider value={{state,dispatch}}>
-            {children}
-        </TodoContext.Provider>
-    )
-}
-
+export const TodoProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(todoReducer, initialState);
+  return (
+    <TodoContext.Provider value={{ state, dispatch }}>
+      {children}
+    </TodoContext.Provider>
+  );
+};
